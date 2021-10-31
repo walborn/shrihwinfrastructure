f5eabc38d842e24d321cefd035197a3bb3a2cf56
Senior Pomidor <seniorpomidor@MacBookYuzhakovBB.local>
2021-11-01 01:48:36 +0300

    add request

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
+import React from 'react'
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>
+  const [ result, setResult ] = React.useState({})
+  const handleClick = () => {
+  fetch('https://api.tracker.yandex.net/v2/myself', {
+    headers: {
+      Authorization: 'OAuth AQAAAAA6-xOMAAd5NiluDTEGHEmRl6TfIdlzL2A',
+      'X-Org-ID': '6610725',
+      'Access-Control-Allow-Origin': '*'
+    },
+  })
+    .then(res => res.json())
+    .then(console.log)
+  }
+    
+  return <>
+  <button onClick={handleClick}>Request</button>
+  <pre>{JSON.stringify(result, null, 2)}</pre>
+  </>
 }
 
 export default HomePage
