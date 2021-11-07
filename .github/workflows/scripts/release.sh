010a0930ebca1e3c679469c67b9f74c60a7564ea
walborn <yuzhakov.boris@gmail.com>
2021-11-08 00:43:25 +0300

    I don't know why github actions don't see git tag list

--- .github/workflows/scripts/release.sh
+++ .github/workflows/scripts/release.sh
@@ -18,62 +18,57 @@ date=$(git show ${tag} | grep Date:)
 commits=$(git log --pretty=format:"%h - %s (%an, %ar)" | tr -s "\n")
 echo -e "# $tag\n$commits\n$(cat CHANGELOG.md)" > CHANGELOG.md
 
-echo $tag
-echo $commits
+created=$(curl --silent --location --request POST ${issues} \
+--header "$headerAuth" \
+--header "$headerOrgId" \
+--header "$headerContentType" \
+--data-raw '{
+  "queue": "TMP",
+  "summary": "'"codebor/$tag"'",
+  "type": "task",
+  "assignee": "codebor",
+  "description": "'"$commits"'",
+  "unique": "'"codebor/$tag"'"
+}')
 
-# created=$(curl --silent --location --request POST ${issues} \
-# --header "$headerAuth" \
-# --header "$headerOrgId" \
-# --header "$headerContentType" \
-# --data-raw '{
-#   "queue": "TMP",
-#   "summary": "'"codebor/$tag"'",
-#   "type": "task",
-#   "assignee": "codebor",
-#   "description": "'"$commits"'",
-#   "unique": "'"codebor/$tag"'"
-# }')
+status=$(echo "$created" | jq -r '.statusCode')
 
-# status=$(echo "$created" | jq -r '.statusCode')
+if [ $status = 201 ]; then
+  echo "Release created successfully"
+elif [ $status = 404 ]; then
+  echo "Not found"
+elif [ $status = 409 ]; then
+  echo "Cannot create task with the same release version"
+  echo "Adding new comment then"
 
-# if [ $status = 201 ]; then
-#   echo "Release created successfully"
-# elif [ $status = 404 ]; then
-#   echo "Not found"
-# elif [ $status = 409 ]; then
-#   echo "Cannot create task with the same release version"
-#   echo "Adding new comment then"
+  taskId=$(curl --silent --location --request POST ${issues}_search \
+    --header "$headerOrgId" \
+    --header "$headerAuth" \
+    --header "$headerContentType" \
+    --data-raw '{
+        "filter": {
+          "unique": "'"codebor/${tag}"'"
+        }
+      }' | jq -r '.[0].key')
 
-#   taskId=$(curl --silent --location --request POST ${issues}_search \
-#     --header "$headerOrgId" \
-#     --header "$headerAuth" \
-#     --header "$headerContentType" \
-#     --data-raw '{
-#         "filter": {
-#           "unique": "'"codebor/${tag}"'"
-#         }
-#       }' | jq -r '.[0].key')
+  updated=$(curl --location --request PATCH ${issues}${taskId} \
+  --header "$headerAuth" \
+  --header "$headerOrgId" \
+  --header "$headerContentType" \
+  --data-raw '{
+    "summary": "'"codebor/$tag"'",
+    "type": "task",
+    "assignee": "codebor",
+    "description": "'"$commits"'"
+  }')
 
-#   updated=$(curl --location --request PATCH ${issues}${taskId} \
-#   --header "$headerAuth" \
-#   --header "$headerOrgId" \
-#   --header "$headerContentType" \
-#   --data-raw '{
-#     "summary": "'"codebor/$tag"'",
-#     "type": "task",
-#     "assignee": "codebor",
-#     "description": "'"$commits"'"
-#   }')
+  status=$(echo "$updated" | jq -r '.statusCode')
 
-#   status=$(echo "$updated" | jq -r '.statusCode')
-
-#   if [ $status = 200 ]; then
-#     echo "Task updated"
-#   elif [ $status = 404 ]; then
-#     echo "Task not found"
-#   elif [ $status = 422 ]; then
-#     echo "Unable to process the contained instructions"
-#   fi
-# else
-#   echo $created
-# fi
+  if [ $status = 200 ]; then
+    echo "Task updated"
+  elif [ $status = 404 ]; then
+    echo "Task not found"
+  elif [ $status = 422 ]; then
+    echo "Unable to process the contained instructions"
+  fi
+fi
